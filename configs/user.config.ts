ca9f650c613793d49f793d3d2629828b27804287
ek.sf <erickoong.secondfloor@gmail.com>
2026-08-13 17:29:05 +0800

    更新網站設定

--- configs/user.config.ts
+++ configs/user.config.ts
@@ -1,12 +1,12 @@
 import type { UserConfig } from "../src/site.config";
 
 const userConfig: UserConfig = {
-  title: "Lipi",
+  title: "不是你的錯",
   description:
-    "A minimal editorial theme for Astro built for blogs, journals, travel writing, and long-form publishing.",
+    "你用最純淨的樣子來到我身邊，所以我知道不是你的錯",
 
-  url: "https://astro-lipi.pages.dev",
-  author: "John Doe",
+  url: "https://my-site-8sm.pages.dev/",
+  author: "LJK",
 
   logo: "/logo.svg",
   avatar: "/avatar.png",
@@ -21,27 +21,27 @@ const userConfig: UserConfig = {
     { title: "RSS", url: "/rss.xml" },
     { title: "Archive", url: "/archive" },
     { title: "Source", url: "https://github.com/thelocalhoststudio/lipi" },
-    
+
   ],
 
-  social: [
-    {
-      title: "GitHub",
-      url: "https://github.com/thelocalhoststudio/lipi",
-      icon: "github",
-    },
-    {
-      title: "X",
-      url: "https://x.com/",
-      icon: "x",
-    },
-    {
-      title: "LinkedIn",
-      url: "https://linkedin.com/",
-      icon: "linkedin",
-    },
-    
-  ],
+  // social: [
+  //   {
+  //     title: "GitHub",
+  //     url: "https://github.com/thelocalhoststudio/lipi",
+  //     icon: "github",
+  //   },
+  //   {
+  //     title: "X",
+  //     url: "https://x.com/",
+  //     icon: "x",
+  //   },
+  //   {
+  //     title: "LinkedIn",
+  //     url: "https://linkedin.com/",
+  //     icon: "linkedin",
+  //   },
+
+  // ],
 
   footerCredits: "Designed for reading. Built with Astro & Lipi",
 
